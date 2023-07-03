867d86dfd747af138fe0d2163bb16e0e9752cbbc
Sumanta Das <dsumanta31746@gmail.com>
2023-07-03 12:46:56 +0530

    calorie intake UI is completed

--- app/src/main/java/com/example/chatapp/CalorieIntake.kt
+++ app/src/main/java/com/example/chatapp/CalorieIntake.kt
@@ -5,21 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 
 class CalorieIntake : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private lateinit var mySpinner: Spinner
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_calorie_intake, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mySpinner = view.findViewById(R.id.mySpinner)
+
+        val options = arrayOf("Breakfast", "Lunch", "Dinner")
+
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, options)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        mySpinner.adapter = adapter
+
+
+    }
 }
